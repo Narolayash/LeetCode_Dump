d946eb73c0e4f20fa75e20e44065be13bee1e5d1
Narola Yash <yashnarola2525@gmail.com>
2025-12-22 17:50:45 +0530

    day2

--- Problem_26.java
+++ Problem_26.java
@@ -1,9 +1,9 @@
-public class Problem_28 {
+public class Problem_26 {
     public static void main(String[] args) {
         int[] nums1 = {1, 1, 2};
         int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
 
-        Solution_26 obj = new Solution_28();
+        Solution_26 obj = new Solution_26();
 
         System.out.println(obj.removeDuplicates(nums));
         System.out.println(obj.removeDuplicates(nums1));
