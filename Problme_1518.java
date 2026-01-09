3d4d7b32e92dac196dfcd7f3b28defd38fbbc6ea
Narola Yash <yashnarola2525@gmail.com>
2026-01-09 22:15:02 +0530

    day 18

---
+++ Problme_1518.java
@@ -0,0 +1,32 @@
+public class Problme_1518 {
+    public static void main(String[] args) {
+        Solution_1518 obj = new Solution_1518();
+
+        System.out.println(obj.numWaterBottles(15, 4));
+    }
+}
+
+//class Solution_1518 {
+//    public int numWaterBottles(int numBottles, int numExchange) {
+//        int drink = numBottles;
+//        while(numBottles >= numExchange) {
+//            numBottles -= numExchange;
+//            drink++;
+//            numBottles++;
+//        }
+//        return drink;
+//    }
+//}
+
+class Solution_1518 {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int drink = numBottles;
+        while(numBottles >= numExchange) {
+            int remainBottles = numBottles % numExchange;
+            drink += numBottles / numExchange;
+            numBottles = (numBottles / numExchange) + remainBottles;
+
+        }
+        return drink;
+    }
+}
