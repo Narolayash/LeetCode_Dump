bfafd9a779866f8bd8c881f234122048b1b0810c
Narola Yash <yashnarola2525@gmail.com>
2026-02-23 21:28:48 +0530

    ...

--- Problem_852.java
+++ Problem_852.java
@@ -7,15 +7,16 @@ public class Problem_852 {
     public static int peakIndexInMountainArray(int[] arr) {
         int start = 0, end = arr.length - 1;
 
-        while (start <= end) {
+        while (start < end) {
             int mid = start + (end - start) / 2;
 
-            if (arr[mid] > arr[mid + 1]) end = mid;
-            else if(arr[mid] < arr[mid + 1]) start = mid + 1;
-            if (start == end) return start;
+            if (arr[mid] > arr[mid + 1]) 
+                end = mid;
+            else   
+                start = mid + 1;  
         }
 
-        return -1;
+        return start;
     }
 
 //    public static int peakIndexInMountainArray(int[] arr) {
