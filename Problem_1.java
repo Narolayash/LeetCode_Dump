bbb8258291d23bbdd4913da46e211762123923dd
Narola Yash <yashnarola2525@gmail.com>
2026-04-14 07:10:39 +0530

    ...

--- Problem_1.java
+++ Problem_1.java
@@ -1,23 +1,26 @@
+
 import java.util.HashMap;
 
 public class Problem_1 {
+
     public static void main(String[] args) {
 
     }
 
     // using Hash Map
-//    public static int[] twoSum(int[] numbs, int target) {
-//        HashMap<Integer, Integer> map = new HashMap<>();
-//
-//        for(int i=0; i<numbs.length; i++) {
-//            int temp = target - numbs[i];
-//
-//            if(map.containsKey(temp))
-//                return new int[] { map.get(temp), i };
-//
-//            map.put(numbs[i], i);
-//        }
-//
-//        return new int[]{};
-//    }
+    public static int[] twoSum(int[] numbs, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < numbs.length; i++) {
+            int temp = target - numbs[i];
+
+            if (map.containsKey(temp)) {
+                return new int[]{map.get(temp), i};
+            }
+
+            map.put(numbs[i], i);
+        }
+
+        return new int[]{};
+    }
 }
