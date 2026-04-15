ad5a4573ca8b3a3041c4d98cb3fa98ccc14e17bc
Narola Yash <yashnarola2525@gmail.com>
2026-04-15 19:10:19 +0530

    ...

--- Problem_1.java
+++ Problem_1.java
@@ -7,6 +7,18 @@ public class Problem_1 {
 
     }
 
+//    using loops
+//    public int[] twoSum(int[] nums, int target) {
+//        for (int i = 0; i < (nums.length-1); i++){
+//            for (int j = (i+1); j < nums.length; j++){
+//                if (nums[i] + nums[j] == target){
+//                    return new int[]{i, j};
+//                }
+//            }
+//        }
+//        return null;
+//    }
+
     // using Hash Map
     public static int[] twoSum(int[] numbs, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
