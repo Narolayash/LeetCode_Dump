dae34514b7d6e8911a14f8253e1260f333d0a58f
Narola Yash <yashnarola2525@gmail.com>
2026-06-20 10:53:07 +0530

    D14

---
+++ Problem_414.java
@@ -0,0 +1,23 @@
+public class Problem_414 {
+    public int thirdMax(int[] nums) {
+        long fmax = Long.MIN_VALUE ;
+        long smax = Long.MIN_VALUE;
+        long tmax = Long.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num > fmax) {
+                tmax = smax;
+                smax = fmax;
+                fmax = num;
+            }
+            else if (num > smax && num != fmax) {
+                tmax = smax;
+                smax = num;
+            }
+            else if (num > tmax && num != smax && num != fmax)
+                tmax = num;
+        }
+
+        return tmax == Long.MIN_VALUE ?  (int)fmax: (int)tmax;
+    }
+}
