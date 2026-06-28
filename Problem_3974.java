cbc736834cf86e68956aa9441e6f6131d6a198de
Narola Yash <yashnarola2525@gmail.com>
2026-06-28 17:37:05 +0530

    D19

---
+++ Problem_3974.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class Problem_3974 {
+    public long maxSum(int[] nums, int k, int mul) {
+        int maxElement = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num > maxElement) maxElement = num;
+        }
+
+        int[] counts = new int[maxElement + 1];
+
+        for (int num : nums) {
+            counts[num]++;
+        }
+
+        long totalSum = 0;
+        for (int i=maxElement; i>=1; i--) {
+            while (counts[i] > 0 && k > 0) {
+                if (mul > 1) totalSum += (long) i * mul--;
+                else totalSum += i;
+
+                counts[i]--;
+                k--;
+            }
+        }
+
+        return totalSum;
+    }
+
+
+
+//    public long maxSum(int[] nums, int k, int mul) {
+//        Arrays.sort(nums);
+//
+//        long totalSum = 0;
+//        int i = nums.length - 1;
+//        while (k > 0 && i >= 0) {
+//            if (mul > 1) totalSum += ((long) nums[i] * mul--);
+//            else totalSum += nums[i];
+//            i--;
+//            k--;
+//        }
+//
+//        return totalSum;
+//    }
+}
+
