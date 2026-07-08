3192988f5349e69c0fec63af7ea5cb4fa230aa51
Narola Yash <yashnarola2525@gmail.com>
2026-07-08 16:57:51 +0530

    D2

--- ListNode.java
+++ ListNode.java
@@ -1,19 +1,30 @@
+
 public class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
-      public static void printList(ListNode head) {
-            if (head == null) {
-                  System.out.println("null");
-                  return;
-            }
+    int val;
+    ListNode next;
 
-            while (head != null) {
-                  System.out.println(head.val);
-                  head = head.next;
-            }
-      }
- }
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public static void printList(ListNode head) {
+        if (head == null) {
+            System.out.println("null");
+            return;
+        }
+
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+}
