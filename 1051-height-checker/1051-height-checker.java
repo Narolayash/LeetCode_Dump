dd3a9c68e3b976efe4c2e8082add9f08da045b1e
Narola Yash N <yashnarola2525@gmail.com>
2026-09-13 18:31:44 +0530

    Time: 0 ms (100%), Space: 43 MB (91.04%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,27 +1,18 @@
     class Solution {
         public int heightChecker(int[] heights) {
-            int n = heights.length;
-            int maxEle = 100;
+            int[] count = new int[101];
 
-            int[] expected = new int[n];
-            int[] freq = new int[maxEle + 1];
-
-            for (int i=0; i<n; i++) {
-                freq[heights[i]]++;
+            for (int height : heights) {
+                count[height]++;
             }
 
-            for (int i=1; i<=maxEle; i++) {
-                freq[i] += freq[i - 1];
-            }
-
-            for (int i=n-1; i>=0; i--) {
-                expected[freq[heights[i]] - 1] = heights[i];
-                freq[heights[i]]--;
-            }
-
+            int idx = 0;
             int ans = 0;
-            for (int i=0; i<n; i++) {   
-                if (heights[i] != expected[i]) ans++;
+            for (int i=0; i<101; i++) {
+                while(count[i]--!=0) {
+                    if (heights[idx] != i) ans++;
+                    idx++;
+                }
             }
 
             return ans;
